c84ab662548a870b11a4b5daae46757d3d1b1ce8
Kevin <kiovit14@gmail.com>
2023-11-29 22:11:53 +0100

    Fixed bug that resized div on click anywhere

--- java/script.js
+++ java/script.js
@@ -1,43 +1,21 @@
-
-
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const container = document.querySelector('.container');
+    const resizeHandle = document.querySelector('.resize-handle');
     let isResizing = false;
 
-    container.addEventListener('mousedown', startResize);
-    container.addEventListener('touchstart', startResize);
-
-    function startResize(e) {
+    resizeHandle.addEventListener('mousedown', function (e) {
         isResizing = true;
         document.addEventListener('mousemove', handleMouseMove);
-        document.addEventListener('mouseup', stopResize);
-        document.addEventListener('touchmove', handleTouchMove, { passive: false });
-        document.addEventListener('touchend', stopResize);
-    }
+        document.addEventListener('mouseup', () => {
+            isResizing = false;
+            document.removeEventListener('mousemove', handleMouseMove);
+        });
+    });
 
     function handleMouseMove(e) {
         if (isResizing) {
             container.style.width = e.clientX - container.offsetLeft + 'px';
             container.style.height = e.clientY - container.offsetTop + 'px';
         }
-    }
-
-    function handleTouchMove(e) {
-        if (isResizing && e.touches.length === 1) {
-            e.preventDefault(); // Prevent scrolling
-            const touch = e.touches[0];
-            container.style.width = touch.clientX - container.offsetLeft + 'px';
-            container.style.height = touch.clientY - container.offsetTop + 'px';
-        }
-    }
-
-    function stopResize() {
-        isResizing = false;
-        document.removeEventListener('mousemove', handleMouseMove);
-        document.removeEventListener('mouseup', stopResize);
-        document.removeEventListener('touchmove', handleTouchMove);
-        document.removeEventListener('touchend', stopResize);
     }
 });
