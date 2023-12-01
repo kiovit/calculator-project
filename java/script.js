5509bc2e3e7ee17ccb587899c558322e01592356
Kevin <kiovit14@gmail.com>
2023-12-01 02:31:01 +0100

    Completed logic for operations with only one operator

--- java/script.js
+++ java/script.js
@@ -1,11 +1,54 @@
 let numberA = 0;
 let numberB = 0;
 let operator = "";
+let input = [];
 let displayResult = 0;
 
 const result = document.querySelector("p");
 result.textContent = displayResult;
+const userInput = document.querySelector(".userInput");
+userInput.textContent = 0;
 
+function clearCalculator(){
+     numberA = 0;
+     numberB = 0;
+     operator = "";
+     input = [];
+     displayResult = 0;
+    updateDisplay();    
+}
+
+function recordInput(value){
+  input.push(value);
+  userInput.textContent = input.join("");
+}
+
+function equalButton(input){
+    const stringIndex = input.findIndex(item => typeof item === 'string' && item !='.');
+    if (stringIndex === -1) {
+      // If there's no string, return the original array
+      return {
+        numberA: input,
+        numberB: []
+      };
+    }
+  
+    const numberAString = input
+      .slice(0, stringIndex)
+      .filter(item => typeof item === 'number' || (typeof item === 'string' && /[0-9.,]/.test(item)))
+      .join("");
+    const numberBString = input
+      .slice(stringIndex + 1)
+      .filter(item => typeof item === 'number' || (typeof item === 'string' && /[0-9.,]/.test(item)))
+      .join("");
+  
+    numberA = parseFloat(numberAString);
+    numberB = parseFloat(numberBString);
+    operator = input[stringIndex];
+
+        operate(operator);
+      }
+ 
 function add(){
     displayResult = numberA + numberB;
     result.textContent = displayResult;
@@ -24,15 +67,14 @@ function multiply(){
 function divide(){
     if (numberB !== 0) {
         displayResult = numberA / numberB;
-        result.textContent = displayResult;
+        result.textContent = displayResult.toFixed(2);
     } else {
         displayResult = "Error";
-        result.textContent = displayResult;
+        result.textContent = displayResult.toFixed(2);
     }
 }
 
-
- function operate(){
+ function operate(operator){
     if (operator === "+"){
         add();
     }else if (operator === "-"){ 
@@ -43,4 +85,9 @@ function divide(){
     divide();
     }
  }
- operate();
+
+ function updateDisplay() {
+    result.textContent = displayResult;
+    userInput.textContent = 0;    
+};
+ 
